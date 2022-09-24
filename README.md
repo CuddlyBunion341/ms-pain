524f73f1223ade747e7c61158955330b7e774dd6
CuddlyBunion341 <daniel@bengl.cz>
2022-09-24 22:26:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ms-pain
+Microsoft Paint but the t is silent.
+A simple clone of the iconic graphics editor build using electron.
+Further documentation pending...
+
+## Installation
+```bash
+npm i
+```
+
+## Development
+```bash
+npm start
+```
+
+## Building
+```bash
+npm run dist
+```
---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+try {
+	require("electron-reloader")(module);
+} catch (_) {}
+
+const createWindow = () => {
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			enableRemoteModule: true,
+		},
+	});
+
+	mainWindow.loadFile("src/index.html");
+
+	mainWindow.webContents.openDevTools();
+};
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") app.quit();
+});
